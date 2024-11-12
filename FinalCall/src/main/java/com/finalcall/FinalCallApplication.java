963c93d2e330ca8948add3a7b7457a86a51846d2
Nick B <nick@nickb364@my.yorku.ca>
2024-11-11 21:15:44 -0500

    barebones Spring Boot application

---
+++ FinalCall/src/main/java/com/finalcall/FinalCallApplication.java
@@ -0,0 +1,13 @@
+package com.finalcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalCallApplication.class, args);
+	}
+
+}
---
+++ FinalCall/src/test/java/com/finalcall/FinalCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
