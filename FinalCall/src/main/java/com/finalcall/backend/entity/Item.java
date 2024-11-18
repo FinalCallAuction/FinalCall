530ae1529b88b3f04c584ae55d685fcb769d07e7
HarrishElango <58488367+HarrishElango@users.noreply.github.com>
2024-11-18 01:11:00 -0500

    Create Item.java

---
+++ FinalCall/src/main/java/com/finalcall/backend/entity/Item.java
@@ -0,0 +1,28 @@
+// src/main/java/com/finalcall/backend/entity/Item.java
+
+package com.finalcall.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    // Additional fields like description, image URL can be added here
+}
