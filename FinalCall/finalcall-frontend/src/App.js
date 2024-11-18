4d51635b0313c96b830813855dd7cd7bffdee279
HarrishElango <58488367+HarrishElango@users.noreply.github.com>
2024-11-18 01:17:54 -0500

    Update App.js

--- FinalCall/finalcall-frontend/src/App.js
+++ FinalCall/finalcall-frontend/src/App.js
@@ -1,5 +1,3 @@
-// src/App.js
-
 import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -7,6 +5,9 @@ import LandingPage from './components/LandingPage';
 import Login from './components/Login';
 import Register from './components/Register';
 import ItemsPage from './components/ItemsPage';
+import CartPage from './components/CartPage'; // Import CartPage
+import PaymentForm from './components/PaymentForm'; // Import PaymentForm
+import ConfirmationPage from './components/ConfirmationPage'; // Import ConfirmationPage
 import { AuthContext } from './context/AuthContext';
 
 function App() {
@@ -21,6 +22,9 @@ function App() {
         <Route path="/login" element={!user ? <Login /> : <Navigate to="/items" />} />
         <Route path="/signup" element={!user ? <Register /> : <Navigate to="/items" />} />
         <Route path="/items" element={<ItemsPage />} />
+        <Route path="/cart" element={<CartPage />} /> {/* Add CartPage */}
+        <Route path="/payment-form" element={<PaymentForm />} /> {/* Add PaymentForm */}
+        <Route path="/confirmation" element={<ConfirmationPage />} /> {/* Add ConfirmationPage */}
         {/* Add a catch-all route for undefined paths */}
         <Route path="*" element={<Navigate to="/items" />} />
       </Routes>
