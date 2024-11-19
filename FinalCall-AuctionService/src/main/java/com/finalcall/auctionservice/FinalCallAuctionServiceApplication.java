5a65b6196510d7eb6d54258972297c4b8b678213
MohamedAhmed74 <moe.ahmed74@gmail.com>
2024-11-18 22:28:17 -0500

    Application for AuctionServices

--- FinalCall-AuctionService/src/main/java/com/finalcall/auctionservice/FinalCallAuctionServiceApplication.java
+++ FinalCall-AuctionService/src/main/java/com/finalcall/auctionservice/FinalCallAuctionServiceApplication.java
@@ -1,7 +1,13 @@
+/**
+ * The main entry point for the Spring Boot application.
+ * Bootstraps the application and enables specific features.
+ */
 package com.finalcall.auctionservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class FinalCallAuctionServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinalCallAuctionServiceApplication.class, args);
