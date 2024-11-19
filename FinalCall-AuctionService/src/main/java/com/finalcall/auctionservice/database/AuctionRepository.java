de6bb478c7ea1981510923f7261793f2811926c7
MohamedAhmed74 <moe.ahmed74@gmail.com>
2024-11-18 22:13:10 -0500

    database access for AuctionService

--- FinalCall-AuctionService/src/main/java/com/finalcall/auctionservice/database/AuctionRepository.java
+++ FinalCall-AuctionService/src/main/java/com/finalcall/auctionservice/database/AuctionRepository.java
@@ -1,2 +1,24 @@
-package com.finalcall.auctionservice.database;public class AuctionRepository {
+/**
+ * Data access layer for the Auction entity.
+ * Extends JpaRepository to provide CRUD operations on auctions.
+ * Defines custom query methods based on auction attributes.
+ */
+
+package com.finalcall.auctionservice.database;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.finalcall.auctionservice.model.Auction;
+import com.finalcall.auctionservice.model.Auction.AuctionStatus;
+import com.finalcall.auctionservice.model.Auction.AuctionType;
+
+public interface AuctionRepository extends JpaRepository<Auction, Long> {
+    List<Auction> findByStatus(AuctionStatus status);
+
+    List<Auction> findByType(AuctionType type);
+
+    Optional<Auction> findByItemId(Long itemId);
+
 }
--- FinalCall-AuctionService/src/main/java/com/finalcall/auctionservice/database/BidRepository.java
+++ FinalCall-AuctionService/src/main/java/com/finalcall/auctionservice/database/BidRepository.java
@@ -1,2 +1,23 @@
-package com.finalcall.auctionservice.database;public class BidRepository {
+/**
+ * Data access layer for the Bid entity.
+ * Extends JpaRepository to provide CRUD operations on bids.
+ * Defines custom query methods based on bid attributes.
+ */
+
+package com.finalcall.auctionservice.database;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finalcall.auctionservice.model.Bid;
+
+public interface BidRepository extends JpaRepository<Bid, Long> {
+    List<Bid> findByAuctionId(Long auctionId);
+
+    Optional<Bid> findTopByAuctionIdOrderByAmountDesc(Long auctionId);
+
+    List<Bid> findAllByBidderId(Long bidderId);
+
 }
