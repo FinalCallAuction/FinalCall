af58645a8b901649f9f6ba6a27e10dff145ac4cb
HarrishElango <58488367+HarrishElango@users.noreply.github.com>
2024-12-17 22:26:34 -0500

    Update App.js

--- FinalCall-Frontend/finalcall-frontend/src/App.js
+++ FinalCall-Frontend/finalcall-frontend/src/App.js
@@ -13,6 +13,10 @@ import ItemDetail from './components/ItemDetail';
 import ChangePassword from './components/ChangePassword';
 import ChangeAddress from './components/ChangeAddress';
 import MyBids from './components/MyBids';
+import PrivateRoute from './components/PrivateRoute';
+import MakePayment from './components/payment/MakePayment';
+import PaymentConfirmation from './components/payment/PaymentConfirmation';
+import PaymentHistory from './components/payment/PaymentHistory';
 
 const App = () => {
   const { user } = useContext(AuthContext);
@@ -23,18 +27,18 @@ const App = () => {
       <Routes>
         <Route path="/" element={<ItemsPage />} />
         <Route path="/items" element={<ItemsPage />} />
+        <Route path="/items/:id" element={<ItemDetail />} />
         <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
-		<Route path="/items/:id" element={<ItemDetail />} />
         <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
         <Route path="/callback" element={<Callback />} />
-		<Route path="/profile/:userId" element={<Profile />} />
-		<Route path="/change-address" element={user ? <ChangeAddress /> : <Navigate to="/login" />} />
-		<Route path="/change-password" element={user ? <ChangePassword /> : <Navigate to="/login" />} />
-		<Route path="/my-bids" element={user ? <MyBids /> : <Navigate to="/login" />} />
-        
-        {/* Protected route for create-item */}
+        <Route path="/profile/:userId" element={<Profile />} />
+        <Route path="/change-address" element={user ? <ChangeAddress /> : <Navigate to="/login" />} />
+        <Route path="/change-password" element={user ? <ChangePassword /> : <Navigate to="/login" />} />
+        <Route path="/my-bids" element={user ? <MyBids /> : <Navigate to="/login" />} />
         <Route path="/create-item" element={user ? <CreateItem /> : <Navigate to="/login" />} />
-
+        <Route path="/make-payment/:auctionId" element={<PrivateRoute><MakePayment /></PrivateRoute>} />
+        <Route path="/payment-confirmation/:transactionId" element={<PrivateRoute><PaymentConfirmation /></PrivateRoute>} />
+        <Route path="/payment-history" element={<PrivateRoute><PaymentHistory /></PrivateRoute>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
