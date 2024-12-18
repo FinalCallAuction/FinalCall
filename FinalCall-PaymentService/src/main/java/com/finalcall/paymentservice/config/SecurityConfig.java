e69bde289f1e272f3772076f628146d9cc96be2a
HarrishElango <58488367+HarrishElango@users.noreply.github.com>
2024-12-17 21:47:58 -0500

    Update SecurityConfig.java

--- FinalCall-PaymentService/src/main/java/com/finalcall/paymentservice/config/SecurityConfig.java
+++ FinalCall-PaymentService/src/main/java/com/finalcall/paymentservice/config/SecurityConfig.java
@@ -1,20 +1,25 @@
 package com.finalcall.paymentservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private CorsConfigurationSource corsConfigurationSource;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(Customizer.withDefaults())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/payments/public/**").permitAll()
