21073762294129cd9857a858c50d2409499496d5
HarrishElango <58488367+HarrishElango@users.noreply.github.com>
2024-12-18 06:00:01 -0500

    Update FinalCallPaymentServiceApplication.java

--- FinalCall-PaymentService/src/main/java/com/finalcall/paymentservice/FinalCallPaymentServiceApplication.java
+++ FinalCall-PaymentService/src/main/java/com/finalcall/paymentservice/FinalCallPaymentServiceApplication.java
@@ -2,6 +2,8 @@ package com.finalcall.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients // Enable Feign clients
