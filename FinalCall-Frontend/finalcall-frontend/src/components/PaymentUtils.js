af5f983027c79e04e1f587ee83b789b28f1f71c7
HarrishElango <58488367+HarrishElango@users.noreply.github.com>
2024-12-18 04:06:06 -0500

    Delete FinalCall-Frontend/finalcall-frontend/src/components/PaymentUtils.js

--- FinalCall-Frontend/finalcall-frontend/src/components/PaymentUtils.js
+++
@@ -1,9 +0,0 @@
-export const formatCreditCard = (number) => {
-  return `**** **** **** ${number.slice(-4)}`;
-};
-
-export const validateCreditCard = {
-  number: (number) => /^[0-9]{16}$/.test(number),
-  expiry: (expiry) => /^(0[1-9]|1[0-2])\/([0-9]{2})$/.test(expiry),
-  cvv: (cvv) => /^[0-9]{3,4}$/.test(cvv)
-};
